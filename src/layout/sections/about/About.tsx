0cba7c692068725b5af93657e4ba6d3c8c18c73f
hadsome-Droid <xkumbhax@gmail.com>
2023-09-16 21:38:17 +0300

    refactor About block

--- src/layout/sections/about/About.tsx
+++ src/layout/sections/about/About.tsx
@@ -5,33 +5,41 @@ import PhotoAbout from '../../../assets/images/projects/photo-about.webp'
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Icon} from "../../../components/icon/Icon";
 import {Container} from "../../../components/Container";
+import EllipseGradient from "../../../assets/images/EllipseGradient.svg"
+
+const listItem = ['I was born in Iran-Mashhad',
+    'I’m 20 years old',
+    'I have started my interest in this field from 2019',
+    'I’m designer , video editor , web developer and ...',
+    'My phone number in Iran +989212073348',]
 
 export const About = () => {
     return (
         <StyledAbout>
             <Container>
-                <FlexWrapper>
-                        <FlexWrapper>
-                            <a href=""><Icon iconId={'wa-white'} viewBox={'2 3 87 87'}/></a>
-                            <a href=""><Icon iconId={'instagram'}/></a>
+                <AboutInfo>
+                        <FlexWrapper content={'end'}>
+                            <a href=""><Icon iconId={'wa-white'} viewBox={'20 20 83 83'}/></a>
+                            <a href=""><Icon iconId={'instagram'} viewBox={'-1 0 47 47'}/></a>
                             <a href=""><Icon iconId={'arrow-top'}/></a>
                         </FlexWrapper>
                         <FlexWrapper direction={'column'}>
-                            <StyledAboutTitle>I’m Alireza
+                            <StyledAboutTitle>I’m <span>Alireza</span>
                                 Kavousy nezjad</StyledAboutTitle>
-                            <ul>
-                                <li>I was born in Iran-Mashhad</li>
-                                <li>I’m 20 years old</li>
-                                <li>I have started my interest in this field from 2019</li>
-                                <li>I’m designer , video editor , web developer and ...</li>
-                                <li>My phone number in Iran +989212073348</li>
-                            </ul>
+                            <AboutList>
+
+                                {listItem.map((listElem)=> {
+                                    return(
+                                        <AboutListItem>{listElem}</AboutListItem>
+                                    )
+                                })}
+                            </AboutList>
                         </FlexWrapper>
-                </FlexWrapper>
-                {/*<WrapperImage>*/}
-                {/*    <img src={Map} alt="#"/>*/}
-                {/*    <img src={PhotoAbout} alt="#"/>*/}
-                {/*</WrapperImage>*/}
+                </AboutInfo>
+                <WrapperImage>
+                    {/*<MapImage src={Map} alt="#"/>*/}
+                    <PhotoImage src={PhotoAbout} alt="#"/>
+                </WrapperImage>
             </Container>
         </StyledAbout>
     );
@@ -42,8 +50,11 @@ const StyledAbout = styled.section`
   
 
   ${Container} {
+    display: flex;
+    justify-content: end;
+    align-items: end;
     position: relative;
-    height: 946px;
+    height: 100%;
     background-color: #A6BCFA;
     
     z-index: 0;
@@ -53,41 +64,97 @@ const StyledAbout = styled.section`
       position: absolute;
       display: inline-block;
       width: 800px;
-      height: 800px;
+      height: 100%;
       background-image: url(${Map});
       background-repeat: no-repeat;
-      background-size: cover;
-      //top: 70px;
-      //left: 41px;
+      background-size: contain;
+      top: 83px;
+      left: 119px;
       z-index: -1;
     }
 
-    &:after {
-      content: '';
-      position: absolute;
-      display: block;
-      width: 100%;
-      height: 100%;
-      background-image: url(${PhotoAbout});
-      background-repeat: no-repeat;
-      background-size: cover;
-
-      top: 0;
-      left: 620px;
-      z-index: -1;
-    }
   }
 `
-// const WrapperImage = styled.div`
-//   display: flex;
-//   overflow: hidden;
-//
-//
-//   img {
-//     width: 100%;
-//     height: 100%;
-//   }
-// `
-const StyledAboutTitle = styled.h2`
+const AboutInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 491px;
+  border-radius: 100px 0;
+  border: 2px solid #FFF;
+  position: relative;
+  margin-bottom: 56px;
+  margin-left: 10px;
+  overflow: hidden;
+  padding-left: 30px;
+  
+  z-index: 0;
 
+  & div:first-child{
+    width: 100%;
+    padding-top: 30px;
+  }
+
+  &:before {
+    content: '';
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    backdrop-filter: blur(10px);
+    z-index: -1;
+  }
+
+`
+
+const WrapperImage = styled.div`
+  width: 800px;
+  overflow: hidden;
+
+
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`
+// const MapImage = styled.img`
+//   //position: absolute;
+// `
+const PhotoImage = styled.img`
+
+`
+
+const StyledAboutTitle = styled.h2`
+  display: inline-block;
+  max-width: 463px;
+  color: #393939;
+  font-size: 60px;
+  font-weight: 700;
+  
+`
+const AboutList = styled.ul`
+ //list-style-image: ${EllipseGradient};
+  
+`
+const AboutListItem = styled.li`
+  color: #393939;
+  font-size: 20px;
+  font-weight: 400;
+  padding-bottom: 10px;
+  margin: 0 auto;
+  
+  &:before{
+    content: '';
+    display: inline-block;
+    width: 18px;
+    height: 18px;
+    border-radius: 50%;
+    background: linear-gradient(180deg, #A6BCFA 0%, #2157F2 100%), #D9D9D9;
+    margin-right: 13px;
+  ;
+  }
+  
 `
